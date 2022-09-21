bda6fa2f3537e69b571e26a2be572b09b66f10d1
Marcus Vinicius <eumarcusviniciusp@gmail.com>
2022-09-20 22:27:41 -0300

    mudei os nomes das variaveis e funções

--- calculator/calc_basic.c
+++ calculator/calc_basic.c
@@ -3,22 +3,22 @@
 
 int main(){
 
-    int num1,num2,soma,sub,mult,div;
+    int num1,num2,sum,sub,mult,div;
 
-    printf("digite o primeiro numero:\n");
+    printf("enter the first integer:\n");
     scanf("%i",&num1);
-    printf("digite o segundo numero:\n");
+    printf("enter the second whole number:\n");
     scanf("%i",&num2);
 
-    soma = num1 + num2;
+    sum = num1 + num2;
     sub = num1 - num2;
     mult = num1 * num2;
     div = num1/num2;
 
-    printf("resultado da soma e = %i\n",soma);
-    printf("resultado da subtracao e = %i\n",sub);
-    printf("resultado da multiplicacao e = %i\n",mult);
-    printf("resultado da divisao e = %i\n",div);
+    printf("result of the sum and = %i\n",sum);
+    printf("result of subtraction and = %i\n",sub);
+    printf("result of multiplication and = %i\n",mult);
+    printf("result of division and = %i\n",div);
 
     return 0;
   
